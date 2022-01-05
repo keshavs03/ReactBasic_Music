76f428a993f1a0c0b19f0515a507bfb379339276
Keshav Somani <75680431+keshavs03@users.noreply.github.com>
2022-01-05 14:22:33 +0530

    adding react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Song from "./components/Song";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Music Player</h1>
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        {/* <FontAwesomeIcon className="skip-back" size="2x" icon={}/> */}
+        <i className="skip-back" class="fas fa-step-backward"></i>
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
